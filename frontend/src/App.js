e09c388e5893ff243d4755640c545d8a108dd2cf
Caio Fernandes <caiovf13@gmail.com>
2020-01-18 13:37:56 -0300

    Add responsiveness to the site

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import './global.css'
 import './App.css'
 import'./Sidebar.css'
+import'./Main.css'
 
 // Componente -> Bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação.
  // Propriedade -> Informações que um componente PAI passa para o componente FILHO.
@@ -41,6 +42,52 @@ function App() {
       </aside>
 
       <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/42192251?s=460&v=4" alt="Caio Fernandes"/>
+              <div className="user-info">
+                <strong>Caio Fernandes</strong>
+                <span>Java, Python, JavaScipt, NodeJS, ReactJS</span>
+              </div>
+            </header>
+            <p>Software Engineering Student at UnB - Universidade de Brasília, Brazil</p>
+            <a href="https://github.com/caiovfernandes">Acesasr perfil no GitHub</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/42192251?s=460&v=4" alt="Caio Fernandes"/>
+              <div className="user-info">
+                <strong>Caio Fernandes</strong>
+                <span>Java, Python, JavaScipt, NodeJS, ReactJS</span>
+              </div>
+            </header>
+            <p>Software Engineering Student at UnB - Universidade de Brasília, Brazil</p>
+            <a href="https://github.com/caiovfernandes">Acesasr perfil no GitHub</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/42192251?s=460&v=4" alt="Caio Fernandes"/>
+              <div className="user-info">
+                <strong>Caio Fernandes</strong>
+                <span>Java, Python, JavaScipt, NodeJS, ReactJS</span>
+              </div>
+            </header>
+            <p>Software Engineering Student at UnB - Universidade de Brasília, Brazil</p>
+            <a href="https://github.com/caiovfernandes">Acesasr perfil no GitHub</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/42192251?s=460&v=4" alt="Caio Fernandes"/>
+              <div className="user-info">
+                <strong>Caio Fernandes</strong>
+                <span>Java, Python, JavaScipt, NodeJS, ReactJS</span>
+              </div>
+            </header>
+            <p>Software Engineering Student at UnB - Universidade de Brasília, Brazil</p>
+            <a href="https://github.com/caiovfernandes">Acesasr perfil no GitHub</a>
+          </li>
+        </ul>
 
       </main>
 
