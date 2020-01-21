3c9e4ab1f84b9f8f36ba5ae57deb9bcbbce1a39f
Caio Fernandes <caiovf13@gmail.com>
2020-01-21 11:34:54 -0300

    Add and configure express CORS to backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes')
+const cors = require('cors');
 
 const app = express();
 
@@ -9,7 +10,8 @@ mongoose.connect('mongodb+srv://omnistack:<>@cluster0-zpcmd.mongodb.net/week10?r
     useUnifiedTopology: true,
 });
 
+app.use(cors({origin: 'http://localhost:3000'}));
 app.use(express.json());
 app.use(routes);
 
-app.listen(3000);
+app.listen(3333);
