1f66029bb264265c5b8a146f6d258d3b4dbf93da
Caio Fernandes <caiovf13@gmail.com>
2020-02-07 11:53:41 -0300

    Finish mobile UI

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Image, View, Text } from 'react-native';
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity } from 'react-native';
 import MapView, { Marker, Callout }  from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+import {MaterialIcons} from  '@expo/vector-icons'
 
-function Main() {
+function Main({ navigation }) {
 
     const [ currentRegion, setCurrentRegion] = useState(null);
 
@@ -34,19 +35,37 @@ function Main() {
     }
 
     return (
-        <MapView initialRegion={ currentRegion } style= { styles.map }>
-            <Marker coordinate= {{ latitude:-15.9981568, longitude:-48.0509952 }} >
-                <Image style={styles.avatar} source={{ uri: 'https://avatars2.githubusercontent.com/u/42192251?s=460&v=4' }}/>
-                
-                <Callout>
-                    <View style={styles.callout}>
-                        <Text style={styles.devName}>Caio Fernandes</Text>
-                        <Text style={styles.devBio}> Software Engineering Student at UnB - Universidade de Brasília, Brazil </Text>
-                        <Text style={styles.Techs}>Java, Python, ReactJS, RectNative</Text>
-                    </View>
-                </Callout>
-            </Marker>
-        </MapView>
+        <>
+            <MapView initialRegion={ currentRegion } style= { styles.map }>
+
+                <Marker coordinate= {{ latitude: currentRegion.latitude, longitude: currentRegion.longitude }} >
+                    <Image style={styles.avatar} source={{ uri: 'https://avatars2.githubusercontent.com/u/42192251?s=460&v=4' }}/>
+                    
+                    <Callout onPress={() => {
+                        //navegação
+                        navigation.navigate('Profile', { github_username: 'caiovfernandes' } );
+                    }}>
+                        <View style={styles.callout}>
+                            <Text style={styles.devName}>Caio Fernandes</Text>
+                            <Text style={styles.devBio}> Software Engineering Student at UnB - Universidade de Brasília, Brazil </Text>
+                            <Text style={styles.Techs}>Java, Python, ReactJS, RectNative</Text>
+                        </View>
+                    </Callout>
+                </Marker>
+            </MapView>
+            <View style={styles.searchForm}>
+                    <TextInput
+                        style={styles.searchInput}
+                        placeholder="Buscar devs por techs..."
+                        placeholderTextColor='#999'
+                        autoCapitalize='words'
+                        autoCorrect={false}
+                    />
+                    <TouchableOpacity onPress={() =>{}} style={styles.loadButton}>
+                        <MaterialIcons name="my-location" size={20} color="#FFF" />
+                    </TouchableOpacity>
+            </View>
+        </>
     );
 }
 
@@ -75,7 +94,41 @@ const styles = StyleSheet.create({
     },
     devTechs:{
         marginTop: 5
-    }
+    },
+    searchForm:{
+        position: 'absolute',
+        top: 20,
+        left: 20,
+        right: 20,
+        zIndex: 5,
+        flexDirection: 'row'
+    },
+    searchInput: {
+        flex:1,
+        height: 50,
+        backgroundColor: '#FFF',
+        color: '#333',
+        borderRadius: 25,
+        paddingHorizontal: 20,
+        fontSize: 16,
+        shadowColor: '#000',
+        shadowOpacity: 0.2,
+        shadowOffset:{
+            width:4,
+            height:4
+        },
+        elevation: 2
+    },
+    loadButton:{
+        width: 50,
+        height:50,
+        backgroundColor: '#8E4DFF',
+        borderRadius: 25,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 15,
+    },
+
 })
 
 export default Main;
--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -21,6 +21,7 @@ const Routes = createAppContainer(
     }, {
         defaultNavigationOptions: {
             headerTintColor: '#FFF',
+            headerBackTitle: false,
             headerStyle: {
                 backgroundColor: '#7D40E7',
             }
